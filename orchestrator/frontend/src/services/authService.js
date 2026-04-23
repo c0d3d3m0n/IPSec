abc0a518cabe7dd52769d68f039b6dd28fa47dd7
kumarmayank09 <mayankmth09@gmail.com>
2026-04-24 01:44:11 +0530

    Persist admin auth token in session storage

--- orchestrator/frontend/src/services/authService.js
+++ orchestrator/frontend/src/services/authService.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 import { ENDPOINTS } from '../config/api';
 
-let accessToken = null;
+const ACCESS_TOKEN_KEY = 'ipsec_admin_access_token';
+let accessToken = sessionStorage.getItem(ACCESS_TOKEN_KEY);
+
+axios.interceptors.request.use((config) => {
+  if (accessToken && config?.headers && !config.headers.Authorization) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
 
 const authService = {
   async login(username, password, totpCode) {
@@ -21,6 +29,7 @@ const authService = {
     });
 
     accessToken = response.data.access_token;
+    sessionStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
     return {
       access_token: response.data.access_token,
       token_type: response.data.token_type,
@@ -36,6 +45,7 @@ const authService = {
 
   logout() {
     accessToken = null;
+    sessionStorage.removeItem(ACCESS_TOKEN_KEY);
     window.location.assign('/login');
   },
 
